20ef3e8e4d2da5e07d08245779c5efd74d6af5b9
miraclebaya <miracleabaya2023@gmail.com>
2023-03-15 11:06:17 +0100

    added task 14 file

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+        System.out.println("This is my first project");
+        System.out.println("switching to software testing");
+        System.out.println("currently studying at testify");
+        System.out.println("this is also my first java project");
+    }
+}
